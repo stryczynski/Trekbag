7eff39f4270f89c4b035b13746228b93f04d2d9b
stryczynski <stryczynski15@gmail.com>
2025-11-13 16:50:13 +0100

    feat: refactor ButtonGroup to use an array for secondary buttons

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,20 @@
 import Button from "./Button";
 
+const secondaryButtons = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items",
+];
+
 export default function ButtonGroup() {
   return (
     <section className="button-group">
-      <Button type="secondary">Mark all as complete</Button>
-      <Button type="secondary">Mark all as incomplete</Button>
-      <Button type="secondary">Reset to initial</Button>
-      <Button type="secondary">Remove all items</Button>
+      {secondaryButtons.map((text) => (
+        <Button key={text} type="secondary">
+          {text}
+        </Button>
+      ))}
     </section>
   );
 }
