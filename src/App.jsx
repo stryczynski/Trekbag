ce6a812d9122aec8229b30c31117356a3385262e
stryczynski <stryczynski15@gmail.com>
2025-11-13 12:50:38 +0100

    feat: initialize Trekbag project with React and Vite
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created main application component (App.jsx) with basic structure including BackgroundHeading and Footer components.
    - Implemented BackgroundHeading component to display the title "Trekbag".
    - Developed Footer component to show copyright information and version number.
    - Added global styles in index.css for layout and design consistency.
    - Set up main entry point (main.jsx) to render the App component.
    - Configured Vite for React application in vite.config.js.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import BackgroundHeading from "./components/BackgroundHeading"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <>
+      <BackgroundHeading />
+
+      <main>
+
+      </main>
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,6 @@
+export default function Footer() {
+  return <footer>
+    <small>&copy; 2050. Copyright by</small>
+    <p>Version <b>1.5</b></p>
+  </footer>
+}
