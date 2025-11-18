f54fdced89c70b460ffd0e9b9f404b5c5a8f4693
stryczynski <stryczynski15@gmail.com>
2025-11-18 15:52:45 +0100

    feat: implement AddItemForm and connect it to Sidebar for item addition

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,9 +1,4 @@
-import { useState } from "react";
-import { initialItems } from "../lib/constants";
-
-export default function ItemList() {
-  const [items, setItems] = useState(initialItems);
-
+export default function ItemList({ items }) {
   return (
     <ul>
       {items.map((item) => (
