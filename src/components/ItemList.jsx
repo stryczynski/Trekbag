9c8fb3441e38984b514a9248d461b60f61498c60
stryczynski <stryczynski15@gmail.com>
2025-11-18 15:19:06 +0100

    feat: implement ItemList component to display a list of initial items

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,3 +1,36 @@
+const initialItems = [
+  {
+    name: "good mood",
+    packed: true,
+  },
+  {
+    name: "passport",
+    packed: false,
+  },
+  {
+    name: "phone charger",
+    packed: false,
+  },
+];
+
 export default function ItemList() {
-  return <div>ItemList</div>;
+  return (
+    <ul>
+      {
+        initialItems.map((item) => (
+          <Item key={item.name} item={item} />
+        ))
+      }
+    </ul>
+  );
 }
+
+const Item = ({ item }) => {
+  return (
+    <li className="item">
+      <label>
+        <input type="checkbox" /> {item.name}
+      </label>
+    </li>
+  );
+};
