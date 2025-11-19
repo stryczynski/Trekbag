728e79329d06c9914710e0c0a6c873ffb0555a27
stryczynski <stryczynski15@gmail.com>
2025-11-19 14:47:25 +0100

    feat: refactor AddItemForm and Sidebar to use handleAddItem for item addition

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,17 @@ import Sidebar from "./Sidebar";
 function App() {
   const [items, setItems] = useState(initialItems);
 
+  const handleAddItem = (newItemText) => {
+    const newItem = {
+      id: new Date().getTime(),
+      name: newItemText,
+      packed: false,
+    };
+
+    const newItems = [...items, newItem];
+    setItems(newItems);
+  };
+
   return (
     <>
       <BackgroundHeading />
@@ -16,7 +27,7 @@ function App() {
       <main>
         <Header />
         <ItemList items={items} />
-        <Sidebar setItems={setItems} />
+        <Sidebar handleAddItem={handleAddItem} />
       </main>
 
       <Footer />
